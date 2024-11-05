8f0279f34c958578f2c9485243aa9ece446e8a6c
norbertocarrillo2005@gmail <norbertocarrillo2005@gmail>
2024-11-05 12:21:26 -0700

    eup

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,10 +1,15 @@
-﻿namespace libreria_ncl.Data
+﻿using libreria_ncl.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace libreria_ncl.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
-        public AppDbContext()
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Book> Books { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,28 +1,27 @@
+using libreria_ncl.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace libreria_ncl
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("DefaultConnectionString");
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
             services.AddRazorPages();
         }
 
